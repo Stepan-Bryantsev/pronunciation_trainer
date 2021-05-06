1a473ffc2f6f28d31f628170e88240aa0ad5b6ff
Stepan-Bryantsev <sabryantsev@edu.hse.ru>
2021-05-06 17:35:34 +0300

    Removed client

--- API/api_client/src/com/pronunciation/AsyncResult.java
+++
@@ -1,6 +0,0 @@
-package com.pronunciation;
-
-public interface AsyncResult<T, V> {
-  void onSuccess(T result);
-  void onFail(V error);
-}
--- API/api_client/src/com/pronunciation/User.java
+++
@@ -1,9 +0,0 @@
-package com.pronunciation;
-
-public class User {
-  private String username;
-  private String first_name;
-  private String last_name;
-  private String email;
-  private Double average_score;
-}

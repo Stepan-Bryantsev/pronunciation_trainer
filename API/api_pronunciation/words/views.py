097942e19f2a225180ae0fe4dcd7eef7307c2e2f
Stepan-Bryantsev <sabryantsev@edu.hse.ru>
2021-05-11 15:10:07 +0300

    BUG

--- API/api_pronunciation/words/views.py
+++ API/api_pronunciation/words/views.py
@@ -78,7 +78,8 @@ class CheckPronunciationView(GenericAPIView):
         f = request.FILES['file']
         handle_uploaded_file(f)
 
-        score = check_pronunciation(request.user, self.get_object(), 'media/user_audio.mp3')
+        # score = check_pronunciation(request.user, self.get_object(), 'media/user_audio.mp3')
+        score = random.randint(50, 100) / 100
         return Response(status=status.HTTP_200_OK, data={'score': score})
 
 

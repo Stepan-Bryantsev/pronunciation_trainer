55e652c8c7928e1ba5b1fd573118f71eff42f28d
KutilinSA <47571746+KutilinSA@users.noreply.github.com>
2021-05-11 16:33:20 +0300

    Update utils.py

--- API/api_pronunciation/analysis/utils.py
+++ API/api_pronunciation/analysis/utils.py
@@ -25,7 +25,7 @@ class ImageComparator:
         for i in range(len(first_hash)):
             if first_hash[i] != second_hash[i]:
                 errors_count += 1
-        return errors_count
+        return errors_count ** 2
 
     @staticmethod
     def compare_images(first_path, second_path):
